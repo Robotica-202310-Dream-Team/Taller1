083621a8547cc33a717a0d19de89df2c94ef03c7
danielvillarg <d.villarg@uniandes.edu.co>
2023-02-07 16:42:02 -0500

    Codigo final Punto 1, movimiento del robot a partir de las teclas

--- taller1_ws/src/turtle_bot_12/turtle_bot_12/turtle_bot_teleop.py
+++ taller1_ws/src/turtle_bot_12/turtle_bot_12/turtle_bot_teleop.py
@@ -1,47 +1,63 @@
 import rclpy
 from rclpy.node import Node
 
+from pynput import keyboard
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 class Turtle_bot_teleop(Node):
 
+    # -----------------------------------------------------INIT--------------------------------------------------------------
+
+
     def __init__(self):
         super().__init__('turtle_bot_teleop')
-        self.velLineal = input("Ingrese la velocidad lineal: ")
-        self.velAngular = input("Ingrese la velocidad angular: ")
-        print(self.velLineal)
-        print(self.velAngular)
-        self.subscription = self.create_subscription(String,'topico_teclas',self.listener_callback,10)
+        self.velLineal = float(input("Ingrese la velocidad lineal: "))
+        self.velAngular = float(input("Ingrese la velocidad angular: "))
         self.publisher = self.create_publisher(Twist, 'turtlebot_cmdVel', 10)
+        self.twist = Twist()
+        self.twist.linear.y = 0.0
+        self.twist.linear.z = 0.0
+        self.twist.angular.x = 0.0
+        self.twist.angular.y = 0.0
+        listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        listener.start()
     
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
-        mensaje = msg.data
-        self.twist = Twist()
-        self.twist.linear.y = 0
-        self.twist.linear.z = 0
-        self.twist.angular.x = 0
-        self.twist.angular.y = 0
-        if mensaje == 'w':
-            self.twist.linear.x = self.velLineal
-            self.twist.angular.z = 0
-        elif mensaje == 'a':
-            self.twist.linear.x = 0
-            self.twist.angular.z = -self.velAngular
-        elif mensaje == 's':
-            self.twist.linear.x = -self.velLineal
-            self.twist.angular.z = 0
-        elif mensaje == 'd':
-            self.twist.linear.x = 0
-            self.twist.angular.z = self.velAngular
-        else:
-            self.twist.linear.x = 0
-            self.twist.angular.z = 0
-        self.publisher.publish(self.twist)
-        
+    # -----------------------------------------------------KEYBOARD THREAD--------------------------------------------------------------
 
+    def on_press(self,key):
+        try:
+            print('alphanumeric key {0} pressed'.format(key.char))
+            if key.char =='w':
+                self.twist.linear.x = self.velLineal
+                self.twist.angular.z = 0.0
+            elif key.char == 'a':
+                self.twist.linear.x = 0.0
+                self.twist.angular.z = -self.velAngular
+            elif key.char == 's':
+                self.twist.linear.x = -self.velLineal
+                self.twist.angular.z = 0.0
+            elif key.char == 'd':
+                self.twist.linear.x = 0.0
+                self.twist.angular.z = self.velAngular
+            self.publisher.publish(self.twist)
+        except AttributeError:
+            print('special key {0} pressed'.format(key))
+            self.twist.linear.x = 0.0
+            self.twist.angular.z = 0.0
+            self.publisher.publish(self.twist)
+
+
+    def on_release(self,key):
+        print('{0} released'.format(key))
+        self.twist.linear.x = 0.0
+        self.twist.angular.z = 0.0
+        self.publisher.publish(self.twist)
+        if key == keyboard.Key.esc:
+            return False
+
+# --------------------------------------------------------MAIN-----------------------------------------------------------
 
 def main(args=None):
     rclpy.init(args=args)
@@ -49,7 +65,6 @@ def main(args=None):
     rclpy.spin(turtle_bot_teleop)
     Turtle_bot_teleop.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
