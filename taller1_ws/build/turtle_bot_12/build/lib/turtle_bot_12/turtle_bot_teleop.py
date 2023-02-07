fbd87895a65b8055f2c7ef2bedb700214df580e2
danielvillarg <d.villarg@uniandes.edu.co>
2023-02-06 21:10:14 -0500

    Avanceeee punto 1

--- taller1_ws/build/turtle_bot_12/build/lib/turtle_bot_12/turtle_bot_teleop.py
+++ taller1_ws/build/turtle_bot_12/build/lib/turtle_bot_12/turtle_bot_teleop.py
@@ -2,36 +2,52 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
-
-class MinimalPublisher(Node):
+class Turtle_bot_teleop(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__('turtle_bot_teleop')
+        self.velLineal = input("Ingrese la velocidad lineal: ")
+        self.velAngular = input("Ingrese la velocidad angular: ")
+        print(self.velLineal)
+        print(self.velAngular)
+        self.subscription = self.create_subscription(String,'topico_teclas',self.listener_callback,10)
+        self.publisher = self.create_publisher(Twist, 'turtlebot_cmdVel', 10)
+    
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        mensaje = msg.data
+        self.twist = Twist()
+        self.twist.linear.y = 0
+        self.twist.linear.z = 0
+        self.twist.angular.x = 0
+        self.twist.angular.y = 0
+        if mensaje == 'w':
+            self.twist.linear.x = self.velLineal
+            self.twist.angular.z = 0
+        elif mensaje == 'a':
+            self.twist.linear.x = 0
+            self.twist.angular.z = -self.velAngular
+        elif mensaje == 's':
+            self.twist.linear.x = -self.velLineal
+            self.twist.angular.z = 0
+        elif mensaje == 'd':
+            self.twist.linear.x = 0
+            self.twist.angular.z = self.velAngular
+        else:
+            self.twist.linear.x = 0
+            self.twist.angular.z = 0
+        self.publisher.publish(self.twist)
+        
 
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    turtle_bot_teleop = Turtle_bot_teleop()
+    rclpy.spin(turtle_bot_teleop)
+    Turtle_bot_teleop.destroy_node()
     rclpy.shutdown()
 
 
