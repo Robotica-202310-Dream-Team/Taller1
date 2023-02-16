1f564452cdf7f3224c08b8dcc11b44b79e7c0852
sebastian guayacan <s.guayacan@uniandes.edu.co>
2023-02-16 17:17:49 -0500

    está el trabajo que hicimos en la sala del ML

--- taller1_ws/src/turtle_bot_12/turtle_bot_12/turtle_bot_interface.py
+++ taller1_ws/src/turtle_bot_12/turtle_bot_12/turtle_bot_interface.py
@@ -7,33 +7,31 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+import turtle
+import tkinter as tk
 
 # Este codigo se suscribe al topico con mensaje tipo Twist de posicon
 # y grafica en tiempo real la posición del robot en un topico con 
 # mensaje tipo Image
-
-global pos_x, pos_y
-global pos_x_total, pos_y_total
 global height, width
 global poses_new, mapa, mapa_actual
 
 #Posicion instantanea del robot (x,y)
-pos_x, pos_y = 0, 0
+
 #Variables para acumular todas las posiciones
-pos_x_total = [0]
-pos_y_total = [0]
-poses_new  =    [0,0]
 mapa_actual = 255*np.ones((500,500),dtype=np.uint8) # imagen en blanco
 mapa = mapa_actual 
 height,width = mapa_actual.shape
 ########################################################################################################################
 
 class MinimalPublisher_suscriber(Node):
-    global pos_x, pos_y
-    global pos_x_total, pos_y_total
     global height, width
-    global poses_new, mapa_actual, mapa
     def __init__(self):
+        self.pos_x = 0
+        self.pos_y = 0
+        self.pos_x_total = [0]
+        self.pos_y_total = [0]
+        self.poses_new  =    [0,0]
         super().__init__('turtle_bot_interface')
         #self.publisher_ = self.create_publisher(String, 'turtle_bot_image', 10) Publica un string
         self.publisher_ = self.create_publisher(Image, 'turtle_bot_image', 10) #publica una imagen
@@ -47,32 +45,90 @@ class MinimalPublisher_suscriber(Node):
         self.mapa_base =  255*np.ones((500,500),dtype=np.uint8)
 
     def subscriber(self, msg):
-        pos_x = msg.linear.x
-        pos_y = msg.linear.y
+        self.pos_x = msg.linear.x
+        self.pos_y = msg.linear.y
         while True:
             #print ("not shutdown")
-            poses_new = pixels((pos_x,pos_y), height, width)
-            pos_x_total.append(poses_new[0])
-            pos_y_total.append(poses_new[1])
+            self.poses_new = pixels((self.pos_x,self.pos_y),height,width)
+            self.pos_x_total.append(self.poses_new[0])
+            self.pos_y_total.append(self.poses_new[1])
             #for i in range(len(pos_y_total)):
-            print (f"x = {pos_x_total[-1]} y = {pos_y_total[-1]}, i= {len(pos_x_total)}")
+            print (f"x = {self.pos_x_total[-1]} y = {self.pos_y_total[-1]}, i= {len(self.pos_x_total)}")
             self.publisher()
                 
-                #image = cv2.circle(mapa_actual, (pos_x_total[i],pos_y_total[i]), radius=5, color=(0, 0, 255), thickness=-1) #color=(Blue,Green,Red)
-                #cv2.imshow('imagen',image)
-                #cv2.waitKey(1)
-
+                
     def publisher(self):
-        #msg = String()
-        #msg.data = 'X = ' + str(pos_x_total[-1]) +'Y = ' + str(pos_y_total[-1]) 
-        #self.publisher_.publish(msg)
-        #self.get_logger().info('Publishing: "%s"' % msg.data)
-        mapa = cv2.circle(self.mapa_base, (pos_x_total[-1],pos_y_total[-1]), radius=5, color=(0, 0, 255), thickness=-1) #color=(Blue,Green,Red)
+        mapa = cv2.circle(self.mapa_base, (self.pos_x_total[-1],self.pos_y_total[-1]), radius=5, color=(0, 0, 255), thickness=-1) #color=(Blue,Green,Red)
         self.mapa_base = mapa
         self.publisher_.publish(self.br.cv2_to_imgmsg(self.mapa_base))
+        #display = Canvas()
+        #display.mover(poses_new)
         
 
 #######################################################################################################################       
+
+class Canvas:
+    def __init__(self):
+        
+        root = tk.Tk()
+        root.title("Turtle_bot_interface Parlington")
+        self.canvas = tk.Canvas(master = root, width = 600, height = 600)
+        self.t = turtle.RawTurtle(self.canvas)
+        self.canvas.pack()
+        self.t.pencolor("black")
+        
+
+        self.nick = tk.StringVar()
+        
+        tk.Label(root, text="Inserte su nick y pulse en Listo!").pack(side = tk.LEFT)
+        self.insert_nick = tk.Entry(root, width=30, textvariable=self.nick).pack(side = tk.LEFT)
+        tk.Button(master = root, text = "Ejemplo de dibujo", command = lambda:self.mover([50,50])).pack(side = tk.LEFT)
+        tk.Button(master = root, text = "Guardar Imagen", command=self.boton2).pack(side = tk.LEFT)
+        tk.Label(master = root, text = "Label").pack(side = tk.LEFT)
+        #minimal_subscriber_publisher = MinimalPublisher_suscriber()
+        print ("minimal_subscriber_publisher")
+        
+        #rclpy.spin(minimal_subscriber_publisher)
+        #minimal_subscriber_publisher.destroy_node()
+        #rclpy.shutdown()
+        root.mainloop()
+
+    def mover(self,pose_new):
+        #t.forward(1)
+        self.t.pencolor("red")
+        self.t.pensize(2)
+        self.t.speed(0)
+        self.t.goto(pose_new[0],pose_new[1])
+    def boton1(self):
+        pass
+    def boton2 (self):
+        print ("Boton2")
+        #img=np.array(self.canvas.canvasx)
+        #ImageTk.PhotoImage(Image.open(img))
+        scriptDir = os.path.dirname(__file__)
+        name =self.nick.get()
+        ruta = scriptDir + "/"+name
+        print (ruta)
+        self.canvas.postscript(file=ruta, colormode='color')
+
+        #cv2.imwrite( ruta,img)
+
+
+
+def main(args=None):
+    global display
+    rclpy.init(args=args)
+    display = Canvas()
+    #minimal_subscriber_publisher = MinimalPublisher_suscriber()
+    print ("minimal_subscriber_publisher")
+    #rclpy.spin(minimal_subscriber_publisher)
+    #minimal_subscriber_publisher.destroy_node()
+    #rclpy.shutdown()
+
+
+##############################################################################################################################
+
+
 
 def pixels (coord,height, width): # Pasar de metros a pixeles 
     x_len = 5 # metros
@@ -84,14 +140,18 @@ def pixels (coord,height, width): # Pasar de metros a pixeles
     return [int(x),int(y)]
     
 def main(args=None):
-    #print('Hi from turtle_bot_12. This node allows us to visualize the real time position of the Turtlebot ')
+    global display
     rclpy.init(args=args)
+    display = Canvas()
     minimal_subscriber_publisher = MinimalPublisher_suscriber()
-    #minimal_subscriber_publisher.timer_callback()
     print ("minimal_subscriber_publisher")
     rclpy.spin(minimal_subscriber_publisher)
     minimal_subscriber_publisher.destroy_node()
     rclpy.shutdown()
+
+
+
+
 
 if __name__ == '__main__':
     main()
